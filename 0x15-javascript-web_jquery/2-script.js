1adc12ab55a0b1beae3d126acefcd1563ba1c90d
Abogo <abogonono1@gmail.com>
2024-08-07 11:55:21 +0100

    A jq script that change color of header to red when hover with mouse 	new file:   0x15-javascript-web_jquery/2-main.html 	new file:   0x15-javascript-web_jquery/2-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+$('header').hover(function () {
+        // over
+        $('header').css('color', '#FF0000');
+    }, function () {
+        // out
+        $('header').css('color', '#000000');
+    }
+);
